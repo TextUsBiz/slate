59adba7f2cf9d46d0b5622bd31104136bf03aae0
Paul Sadauskas <psadauskas@gmail.com>
2017-08-15 10:56:18 -0600

    add middleman livereload

--- config.rb
+++ config.rb
@@ -48,6 +48,8 @@ configure :build do
   # activate :gzip
 end
 
+activate :livereload
+
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
